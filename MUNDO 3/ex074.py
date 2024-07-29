ef88b28300ed792e25da420c4ebe7f59b6d0133b
James Lima <jamesgomesl@hotmail.com>
2024-07-29 02:53:04 -0300

    testando defs no ex074

--- MUNDO 3/ex074.py
+++ MUNDO 3/ex074.py
@@ -1,11 +1,20 @@
 from random import randint
 
+def rolar_dado(inicio, final):
+    return randint(inicio, final)
 
-def gerar_numeros():
-    return randint(0, 99)
+def gerar_numeros(inicio:int, final:int, quantos:int):
+    for c in range (quantos):
+        if c == 0:
+            lista_numeros = rolar_dado(inicio, final)
+        elif c < quantos:
+            lista_numeros += rolar_dado(inicio, final)
+        elif len(lista_numeros) == quantos:
+            return lista_numeros
 
-
-tupla = (gerar_numeros(), gerar_numeros(), gerar_numeros(), gerar_numeros(), gerar_numeros())
+# tupla = gerar_numeros(0, 99, 5)
+tupla = (rolar_dado(0,99), rolar_dado(0,99), rolar_dado(0,99), rolar_dado(0,99), rolar_dado(0,99))
+        
 print(f"""Tupla: {tupla}
 Maior número: {max(tupla)}
 Menor número: {min(tupla)}""")
