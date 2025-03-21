7127cc356ba9cb34c824cf48740b36f24dc5625e
JamesGL <jamesgomesl@hotmail.com>
2025-03-20 23:58:58 -0300

    Starting POO on ex014

---
+++ MUNDO 1/ex014_POO.py
@@ -0,0 +1,56 @@
+class temperatura:
+    def __init__(self, temp_name):
+        self.temp_name = temp_name
+    
+class conversoes:
+    def __init__(self):
+        pass
+    
+    Celsius_to_Kelvin = lambda
+    Celsius_to_Fahrenheit = lambda
+    Fahrenheit_to_Celsius = lambda
+    Fahrenheit_to_Kelvin = lambda
+    Kelvin_to_Celsius = lambda
+    Kelvin_to_Fahrenheit = lambda
+    
+
+
+mensagem_de_entrada = "CONVERSOR DE TEMPERATURAS"
+
+
+print(mensagem_de_entrada)
+
+
+
+"""
+escala_1 = input('Digite a inicial da primeira escala: ')
+temp_1 = float(input('Digite a sua temperatura: '))
+escala_2 = input('Digite a inicial da segunda escala: ')
+
+if escala_1 == 'C':
+    if escala_2 == 'F':
+        C_to_F = temp_1 * 9 / 5 + 32
+        temp_2 = C_to_F  # 30 - 86
+        print('\nConversão de Celsius para Fahrenheit\n'
+              '{} °C = {} °F'.format(temp_1, temp_2))
+    if escala_2 == 'K':
+        C_to_K = temp_1 + 273.15  # C >> K
+        temp_2 = C_to_K
+        print('\nConversão de Celsius para Kelvin\n'
+              '{} °C = {} K'.format(temp_1, temp_2))
+
+if escala_1 == 'F':
+    if escala_2 == 'C':
+        print('')
+    if escala_2 == 'K':
+        print('')
+
+if escala_1 == 'K':
+    if escala_2 == 'C':
+        print('')
+    if escala_2 == 'F':
+        print('')
+
+input('\nPressione ENTER para sair')
+
+"""
